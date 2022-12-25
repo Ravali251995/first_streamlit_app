536ef392e20a0a56fdd11d6974aaacf1c3982208
Ravali251995 <118627271+Ravali251995@users.noreply.github.com>
2022-12-25 11:37:45 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,21 +23,20 @@ streamlit.dataframe(my_fruit_list)
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
-
-
-#import requests
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
-streamlit.header("Fruityvice Fruit Advice!")
-#streamlit.text(fruityvice_response.json())
-
-# normalizing the output to convert to a table
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# print the normalized output
-streamlit.dataframe(fruityvice_normalized)
+#try/except
+streamlit.header('Fruityvice Fruit Advice')
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit for information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
+    # normalizing the output to convert to a table
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # print the normalized output
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 #dont run anything post this command while we troubleshoot
 streamlit.stop()
 #import snowflake.connector
